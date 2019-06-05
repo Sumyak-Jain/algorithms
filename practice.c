380e0fe9c383add5044abc2c1c80d81b5dd70655
sumyak jain <3456gaurav@gmail.com>
2019-06-05 23:24:51 +0530

    even_odd

---
+++ practice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter a number");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("even number");
+    }
+    else
+    {
+        printf("odd number");
+
+    }
+    return 0;
+}
+
