7b23fd1a524412d9827376d0a61bdfd7baf1b3a2
sumyak jain <3456gaurav@gmail.com>
2019-06-06 00:01:49 +0530

    largest of three

--- practice.c
+++ practice.c
@@ -1,3 +1,4 @@
+//program for even and odd
 #include <stdio.h>
 int main()
 {
@@ -11,6 +12,34 @@ int main()
     else
     {
         printf("odd number");
+
+    }
+    return 0;
+}
+//program for largest of three
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter three numbers");
+    scanf("%d%d%d",&a,&b,&c);
+    if((a>b)&&(a>c))
+    {
+        printf("%d is largest",a);
+    }
+    else if((b>a)&&(b>c))
+    {
+        printf("%d is largest",b);
+
+    }
+    else if((c>a)&&(c>b))
+    {
+        printf("%d is largest",c);
+
+    }
+     else
+    {
+        printf("all are equal");
 
     }
     return 0;
